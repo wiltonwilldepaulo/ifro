72e259b2f2c50843211d227065f1bb2dde21e40c
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-03-16 13:57:36 -0400

    Commit

--- public/index.php
+++ public/index.php
@@ -1,17 +1,5 @@
 <?php
 
-use Slim\Factory\AppFactory;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = AppFactory::create();
-
-$app->addRoutingMiddleware();
-
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-require __DIR__ . '/../app/helpers/settings.php';
-
-require __DIR__ . '/../app/routes/routes.php';
+$app = require __DIR__ . '/../app/bootstrap.php';
 
 $app->run();
