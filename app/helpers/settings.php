79ed8b0a9b7541a253521f27101c3a19dfe5af0c
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-03-17 15:42:51 -0400

    jjjjjjjjjjjjj

--- app/helpers/settings.php
+++ app/helpers/settings.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
-define('ROOT', dirname(__FILE__, 3));
+
+defined('ROOT') or define('ROOT', dirname(__FILE__, 3));
 #DIRETÓRIO DAS VIEWS
-define('DIR_VIEWS', ROOT . '/App/View/');
+defined('DIR_VIEWS') or define('DIR_VIEWS', __DIR__ . '/App/View');
 #EXTENSÃO PADRÃO DAS VIEWS
-define('EXT_VIEWS', '.html');
+defined('EXT_VIEWS') or define('EXT_VIEWS', '.html');
